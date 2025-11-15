acb8035505b09ad2d5bd0ef1670aa32442d210be
jodlin <jodlinh@gmail.com>
2025-11-15 11:26:30 +0100

    add collection file width updateFirst method

---
+++ staff/jodlin-huerta/playground/js/collection.js
@@ -0,0 +1,78 @@
+var collection = {
+    count: 0    
+}
+
+
+collection.add = function(item) {
+    this[this.count] = item
+    this.count++    
+}
+
+collection.remove = function(item) {
+    for (var i=0; i < this.count; i++)
+        if(this[i] === item)
+            delete this[i]
+}
+
+collection.removeFirst = function(item) {
+    for (var i=0; i < this.count; i++)
+        if(this[i] === item) {
+            delete this[i]
+            return
+        }
+}
+
+collection.update = function(now, new1){
+    for (var i=0; i < this.count; i++)
+        if(this[i] === now) 
+            this[i] = new1        
+        
+}
+
+
+collection.updateFirst = function(now, new1){
+    for (var i = 0; i < this.count; i++)
+        if (this[i] === now){
+            this[i] = new1 
+            return
+        }
+}
+
+
+
+collection.add('Peter')
+console.log(collection)
+
+// { 0: 'Peter', count: 1 }
+
+collection.add('Wendy')
+console.log(collection)
+// { 0: 'Peter', 1: 'Wendy', count: 2 }
+
+collection.add('James')
+console.log(collection)
+// { 0: 'Peter', 1: 'Wendy', 2: 'James', count: 3 }
+
+collection.remove('Wendy')
+console.log(collection)
+// { 0: 'Peter',  2: 'James', count: 3 }
+
+
+collection.add('Wendy')
+collection.add('Pepito')
+collection.add('Wendy')
+collection.add('Pepito')
+
+console.log(collection)
+// {0: 'Peter', 2: 'James', 3: 'Wendy', 4: 'Pepito', 5: 'Wendy', 6: 'Pepito', count: 7 }
+
+
+//TODO implement update method
+
+collection.update('Pepito', 'Jiminy')
+console.log(collection)
+// {0: 'Peter', 2: 'James', 3: 'Wendy', 4: 'Jiminy', 5: 'Wendy', 6: 'Jiminy', count: 7 }
+
+collection.updateFirst('Jiminy', 'Pepito')
+console.log(collection)
+// {0: 'Peter', 2: 'James', 3: 'Wendy', 4: 'Pepito', 5: 'Wendy', 6: 'Jiminy', count: 7 }
