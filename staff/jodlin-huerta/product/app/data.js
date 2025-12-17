79b1b63f5cf46bf5910e146b27a217434367cdb4
jodlin <jodlinh@gmail.com>
2025-12-17 14:35:20 +0100

    migrate prototype function to classes

--- staff/jodlin-huerta/product/app/data.js
+++ staff/jodlin-huerta/product/app/data.js
@@ -1,105 +1,105 @@
 //model
 
-function User(id, name, email, username, password, role) {
-    this.id = id
-    this.name = name
-    this.email = email
-    this.username = username
-    this.password = password
-    this.role = role
+class User {
+    constructor(id, name, email, username, password, role) {
+        this.id = id
+        this.name = name
+        this.email = email
+        this.username = username
+        this.password = password
+        this.role = role
+    }
 }
 
-function Pet(id, userId, /*chip,*/ name, /*gender,*/ birthday, weight, /* specie, race, colors, */ image) {
-    this.id = id
-    this.userId = userId
-    // this.chip = chip
-    this.name = name
-    //  this.gender = gender
-    this.birthday = birthday
-    this.weight = weight
-    // this.specie = specie
-    // this.race = race
-    // this.colors = colors
-    this.image = image
+class Pet {
+    constructor(id, userId, /*chip,*/ name, /*gender,*/ birthday, weight, /* specie, race, colors, */ image) {
+        this.id = id
+        this.userId = userId
+        // this.chip = chip
+        this.name = name
+        //  this.gender = gender
+        this.birthday = birthday
+        this.weight = weight
+        // this.specie = specie
+        // this.race = race
+        // this.colors = colors
+        this.image = image
+    }
 }
+
 
 //manager
 
-function Data() {
-    this.users = []
-    this.usersCount = 0
-    this.pets = []
-    this.petsCount = 0
-    this.loggedInUserId = null
-
-}
-
-
-Data.prototype.insertUser = function (user) {
-    this.users.push(user)
-    this.usersCount++
-}
-
-
-Data.prototype.findUserByEmail = function (email) {
-    for (let i = 0; i < this.users.length; i++) {
-        const user = this.users[i]
-
-        if (user.email === email) return user
+class Data {
+    constructor() {
+        this.users = []
+        this.usersCount = 0
+        this.pets = []
+        this.petsCount = 0
+        this.loggedInUserId = null
     }
-    return null
-}
 
-Data.prototype.findUserByUsername = function (username) {
-    for (let i = 0; i < this.users.length; i++) {
-        const user = this.users[i]
-
-        if (user.username === username) return user
+    insertUser(user) {
+        this.users.push(user)
+        this.usersCount++
     }
-    return null
-}
 
-Data.prototype.findUserByUserId = function (userId) {
-    for (let i = 0; i < this.users.length; i++) {
-        const user = this.users[i]
-        if (user.id === userId) return user
+    findUserByEmail(email) {
+        for (let i = 0; i < this.users.length; i++) {
+            const user = this.users[i]
+
+            if (user.email === email) return user
+        }
+        return null
     }
-}
 
+    findUserByUsername(username) {
+        for (let i = 0; i < this.users.length; i++) {
+            const user = this.users[i]
 
-
-Data.prototype.setLoggedInUserId = function (userId) {
-    this.loggedInUserId = userId
-}
-
-Data.prototype.getLoggedUserId = function () {
-    return this.loggedInUserId
-}
-
-Data.prototype.insertPet = function (pet) {
-    this.pets.push(pet)
-    this.petsCount++
-}
-
-Data.prototype.findPetsByUserId = function (userId) {
-    const foundPets = []
-    for (let i = 0; i < this.pets.length; i++) {
-        const pet = this.pets[i]
-        if (pet.userId === userId)
-            foundPets.push(pet)
+            if (user.username === username) return user
+        }
+        return null
     }
-    return foundPets
 
-}
-
-Data.prototype.findPetsById = function (petId) {
-    for (let i = 0; i < this.pets.length; i++) {
-        const pet = this.pets[i]
-        if (pet.id === petId)
-            return pet
+    findUserByUserId(userId) {
+        for (let i = 0; i < this.users.length; i++) {
+            const user = this.users[i]
+            if (user.id === userId) return user
+        }
     }
-    return null
 
+    setLoggedInUserId(userId) {
+        this.loggedInUserId = userId
+    }
+
+    getLoggedUserId() {
+        return this.loggedInUserId
+    }
+
+    insertPet(pet) {
+        this.pets.push(pet)
+        this.petsCount++
+    }
+
+    findPetsByUserId(userId) {
+        const foundPets = []
+        for (let i = 0; i < this.pets.length; i++) {
+            const pet = this.pets[i]
+            if (pet.userId === userId)
+                foundPets.push(pet)
+        }
+        return foundPets
+    }
+
+    findPetsById(petId) {
+        for (let i = 0; i < this.pets.length; i++) {
+            const pet = this.pets[i]
+            if (pet.id === petId)
+                return pet
+        }
+        return null
+    }
 }
 
 const data = new Data()
--- staff/jodlin-huerta/product/app/logic.js
+++ staff/jodlin-huerta/product/app/logic.js
@@ -1,123 +1,126 @@
-function Logic() { }
+class Logic {
+    constructor() {
 
-Logic.prototype.registerUser = function (
-    name,
-    email,
-    username,
-    password,
-    passwordRepeat
-) {
+    }
 
-    if (typeof name !== "string") throw new Error("invalid name type");
-    if (name.length < 1) throw new Error("invalid name length");
+    registerUser(
+        name,
+        email,
+        username,
+        password,
+        passwordRepeat
+    ) {
 
-    if (typeof email !== "string") throw new Error("invalid email type");
-    if (email.length < 1) throw new Error("invalid email length");
+        if (typeof name !== "string") throw new Error("invalid name type");
+        if (name.length < 1) throw new Error("invalid name length");
 
-    if (typeof username !== "string") throw new Error("invalid username type");
-    if (username.length < 1) throw new Error("invalid username length");
+        if (typeof email !== "string") throw new Error("invalid email type");
+        if (email.length < 1) throw new Error("invalid email length");
 
-    if (typeof password !== "string") throw new Error("invalid password type");
-    if (password.length < 8) throw new Error("invalid password length");
+        if (typeof username !== "string") throw new Error("invalid username type");
+        if (username.length < 1) throw new Error("invalid username length");
 
-    if (typeof passwordRepeat !== "string") throw new Error("invalid passwordRepeat type");
-    if (passwordRepeat.length < 8) throw new Error("invalid passwordRepeat length");
+        if (typeof password !== "string") throw new Error("invalid password type");
+        if (password.length < 8) throw new Error("invalid password length");
 
-    if (password !== passwordRepeat) throw new Error("password do no match")
+        if (typeof passwordRepeat !== "string") throw new Error("invalid passwordRepeat type");
+        if (passwordRepeat.length < 8) throw new Error("invalid passwordRepeat length");
 
-    let user = data.findUserByEmail(email)
-    if (user !== null) throw new Error("User email already exists")
+        if (password !== passwordRepeat) throw new Error("password do no match")
 
-    user = data.findUserByUsername(username)
-    if (user !== null) throw new Error('username already exists')
+        let user = data.findUserByEmail(email)
+        if (user !== null) throw new Error("User email already exists")
 
-    user = new User('user-' + data.usersCount, name, email, username, password, 'regular')
-    data.insertUser(user)
-};
+        user = data.findUserByUsername(username)
+        if (user !== null) throw new Error('username already exists')
+
+        user = new User('user-' + data.usersCount, name, email, username, password, 'regular')
+        data.insertUser(user)
+    };
 
 
 
-Logic.prototype.loginUser = function (username, password) {
-    if (typeof username !== 'string') throw new Error('Invalid UserName type')
-    if (username.length < 3) throw new Error('Invalid UserName length')
+    loginUser(username, password) {
+        if (typeof username !== 'string') throw new Error('Invalid UserName type')
+        if (username.length < 3) throw new Error('Invalid UserName length')
 
-    if (typeof password !== 'string') throw new Error('Invalid Password type')
+        if (typeof password !== 'string') throw new Error('Invalid Password type')
 
-    let user = data.findUserByUsername(username)
-    if (user === null) throw new Error('UserName not found')
+        let user = data.findUserByUsername(username)
+        if (user === null) throw new Error('UserName not found')
 
-    if (user.password !== password) throw new Error('wrong password')
+        if (user.password !== password) throw new Error('wrong password')
 
-    data.setLoggedInUserId(user.id)
+        data.setLoggedInUserId(user.id)
 
+    }
+
+    logoutUser() {
+        data.setLoggedInUserId(null)
+    }
+
+    addPet(name, birthdate, weight, image) {
+        const userId = data.getLoggedUserId()
+        if (data.findUserByUserId(userId) === null) throw new Error('user not exist')
+        if (userId === null) throw new Error('user not logged in')
+
+
+
+        if (typeof name !== 'string') throw new Error('invalid name type')
+        if (name.length < 1) throw new Error('Invalid name length')
+
+        const isoDateRegex = /^\d{4}-\d{2}-\d{2}$/
+        if (!isoDateRegex.test(birthdate)) throw new Error('Invalid birthday format')
+
+
+        if (typeof weight !== 'number' || isNaN(weight)) throw new Error('Invalid weight type')
+
+        if (typeof image !== 'string') throw new Error('Invalid image type')
+
+        const urlRegex = /(www|http:|https:)+[^\s]+[\w]/
+        if (!urlRegex.test(image)) throw new Error('Invalid image format')
+
+        const pet = new Pet('pet-' + data.petsCount, data.getLoggedUserId(), name, birthdate, weight, image)
+
+        data.insertPet(pet)
+
+    }
+
+
+    getPets() {
+        const userId = data.getLoggedUserId()
+        if (userId === null) throw new Error('user not logged in')
+        if (data.findUserByUserId(userId) === null) throw new Error('user not exist')
+
+        const pets = data.findPetsByUserId(userId)
+
+        return pets
+
+    }
+
+    deletePet(petId) {
+        const userId = data.getLoggedUserId()
+        if (data.findUserByUserId(userId) === null) throw new Error('user not exist')
+        if (userId === null) throw new Error('user not logged in')
+
+        if (typeof petId !== 'string') throw new Error('invalid pet-id type')
+
+        const petIdRegex = /^\pet-[0-9]+$/
+        if (!petIdRegex.test(petId)) throw new Error('invalid pet-id format')
+
+        const pet = data.findPetsById(petId)
+        if (pet === null) throw new Error('pet not found')
+
+        if (pet.userId !== data.getLoggedUserId()) throw new Error('user not owner of pet')
+
+        const petIndex = data.pets.indexOf(pet)
+        data.pets.splice(petIndex, 1)
+
+
+
+
+    }
 }
-
-Logic.prototype.logoutUser = function () {
-    data.setLoggedInUserId(null)
-}
-
-Logic.prototype.addPet = function (name, birthdate, weight, image) {
-    const userId = data.getLoggedUserId()
-    if (data.findUserByUserId(userId) === null) throw new Error('user not exist')
-    if (userId === null) throw new Error('user not logged in')
-
-
-
-    if (typeof name !== 'string') throw new Error('invalid name type')
-    if (name.length < 1) throw new Error('Invalid name length')
-
-    const isoDateRegex = /^\d{4}-\d{2}-\d{2}$/
-    if (!isoDateRegex.test(birthdate)) throw new Error('Invalid birthday format')
-
-
-    if (typeof weight !== 'number' || isNaN(weight)) throw new Error('Invalid weight type')
-
-    if (typeof image !== 'string') throw new Error('Invalid image type')
-
-    const urlRegex = /(www|http:|https:)+[^\s]+[\w]/
-    if (!urlRegex.test(image)) throw new Error('Invalid image format')
-
-    const pet = new Pet('pet-' + data.petsCount, data.getLoggedUserId(), name, birthdate, weight, image)
-
-    data.insertPet(pet)
-
-}
-
-
-Logic.prototype.getPets = function () {
-    const userId = data.getLoggedUserId()
-    if (userId === null) throw new Error('user not logged in')
-    if (data.findUserByUserId(userId) === null) throw new Error('user not exist')
-
-    const pets = data.findPetsByUserId(userId)
-
-    return pets
-
-}
-
-Logic.prototype.deletePet = function (petId) {
-    const userId = data.getLoggedUserId()
-    if (data.findUserByUserId(userId) === null) throw new Error('user not exist')
-    if (userId === null) throw new Error('user not logged in')
-
-    if (typeof petId !== 'string') throw new Error('invalid pet-id type')
-
-    const petIdRegex = /^\pet-[0-9]+$/
-    if (!petIdRegex.test(petId)) throw new Error('invalid pet-id format')
-
-    const pet = data.findPetsById(petId)
-    if (pet === null) throw new Error('pet not found')
-
-    if (pet.userId !== data.getLoggedUserId()) throw new Error('user not owner of pet')
-
-    const petIndex = data.pets.indexOf(pet)
-    data.pets.splice(petIndex, 1)
-
-
-
-
-}
-
 
 
 const logic = new Logic();
